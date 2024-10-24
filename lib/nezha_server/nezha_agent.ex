01e417ad732ca37c7665050cf9c98208b304cba7
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-24 15:29:51 +0800

    Create nezha_agent.ex

---
+++ lib/nezha_server/nezha_agent.ex
@@ -0,0 +1,27 @@
+defmodule NezhaServer.NezhaAgent do
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_) do
+    start_agent()
+    {:ok, %{}}
+  end
+
+  defp start_agent do
+    agent_path = "./agent"
+    File.chmod!(agent_path, 0o755)
+
+    Port.open({:spawn_executable, agent_path},
+      args: [
+        "-s",
+        "tzz.shiyue.eu.org:5555",
+        "-p",
+        "d4Kf1fkj8ALZ8LL6Tk",
+        "-d"
+      ]
+    )
+  end
+end
