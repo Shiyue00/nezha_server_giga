b9e7560f74a89c76e810b08de0d6cb3ecab4dbc0
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-24 15:55:24 +0800

    Update application.ex

--- lib/nezha_server/application.ex
+++ lib/nezha_server/application.ex
@@ -2,7 +2,8 @@ defmodule NezhaServer.Application do
   use Application
 
   def start(_type, _args) do
-    port = Application.get_env(:nezha_server, :port, 4000)
+    # Explicitly use port 4000 for Gigalixir
+    port = 4000
     
     children = [
       {Plug.Cowboy, scheme: :http, plug: NezhaServer.Router, options: [port: port]},
