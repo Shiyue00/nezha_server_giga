3eef20479fbdd447d89111df015d134e9dfeadce
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-24 15:31:36 +0800

    Create mix.exs

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule NezhaServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :nezha_server,
+      version: "0.1.0",
+      elixir: "~> 1.13.0",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {NezhaServer.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:plug_cowboy, "~> 2.5"}
+    ]
+  end
+end
