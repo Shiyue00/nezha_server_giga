ef81805081b641f52a938238e0d0c994cb1e6a0a
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-24 16:02:15 +0800

    Update nezha_agent.ex

--- lib/nezha_server/nezha_agent.ex
+++ lib/nezha_server/nezha_agent.ex
@@ -1,5 +1,6 @@
 defmodule NezhaServer.NezhaAgent do
   use GenServer
+  require Logger
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -11,9 +12,24 @@ defmodule NezhaServer.NezhaAgent do
   end
 
   defp start_agent do
-    agent_path = "./agent"
-    File.chmod!(agent_path, 0o755)
+    # Log the current directory for debugging
+    Logger.info("Current directory: #{File.cwd!()}")
+    
+    # Use absolute path in production
+    agent_path = Path.join(:code.priv_dir(:nezha_server), "agent")
+    Logger.info("Agent path: #{agent_path}")
 
+    case File.chmod(agent_path, 0o755) do
+      :ok -> 
+        Logger.info("Successfully set agent permissions")
+        start_agent_process(agent_path)
+      {:error, reason} ->
+        Logger.warn("Could not set agent permissions: #{inspect(reason)}")
+        {:ok, %{}}
+    end
+  end
+
+  defp start_agent_process(agent_path) do
     Port.open({:spawn_executable, agent_path},
       args: [
         "-s",
