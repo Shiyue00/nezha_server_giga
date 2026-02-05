a6b439319c7f140f4823c0d18066d2fc330473d4
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2026-02-05 16:08:37 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NezhaServer.MixProject do
     [
       app: :nezha_server,
       version: "0.1.0",
-      elixir: "~> 1.13.0",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
